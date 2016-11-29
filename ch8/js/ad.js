29e37630544f22fde5b672849c95f1c1ce625491
ranzhouli <874177815@qq.com>
2016-11-30 00:25:22 +0800

    xiugai

--- ch8/js/ad.js
+++ ch8/js/ad.js
@@ -1,7 +1,30 @@
 $(function(){
+  var index = 0;
+  var $imgrolls = $("#jnImageroll div a");
+  $imgrolls.css("opacity", "0.7");
+  var len = $imgrolls.length;
+  var adTimer = null;
+
+  $imgrolls.on("mouseover", function(){
+    index = $imgrolls.index(this);
+    showImg(index);
+  }).eq(0).mouseover();
+//滑入停止动画，滑出开始动画
+  $("#jnImageroll").hover(function(){
+    if (adTimer) {
+      clearInterval(adTimer);
+    }
+  }, function(){
+    adTimer = setInterval(function(){
+      showImg(index);
+      index++;
+      if (index==len) {
+        index = 0;
+      }
+    }, 5000);
+  });
 
 
-  
   //显示不同的照片
   function showImg(index){
     var $rolllist = $("jnImageroll").find("div a");
