6f0cbe17efbd86f7d8cd9a1322a9f06da472f545
ranzhouli <874177815@qq.com>
2016-12-01 00:56:36 +0800

    ranzhouli ch8 index

--- ch8/js/ad.js
+++ ch8/js/ad.js
@@ -23,16 +23,17 @@ $(function(){
       }
     }, 5000);
   });
+});
 
 
   //显示不同的照片
   function showImg(index){
-    var $rolllist = $("jnImageroll").find("div a");
+    var $rolllist = $("#jnImageroll").find("div a");
     var newhref = $rolllist.eq(index).attr("href");
     $("#JS_imgWrap").attr("href", "newhref")
                     .find("img").eq(index).stop(true,true).fadeIn()
                     .siblings().fadeOut();
+
     $rolllist.removeClass("chos").css("opacity", "0.7")
             .eq(index).addClass("chos").css("opacity", "1");
   }
-});
--- ch8/js/imgHover.js
+++ ch8/js/imgHover.js
@@ -1,1 +1,17 @@
 /*滑过图片出现放大镜效果*/
+$(function(){
+  $("#jnBrandList li").each(function(){
+    var $img = $(this).find("img");
+    //console.log($img);
+    var img_w = $img.width();
+    var img_h = $img.height();
+    console.log(img_w);
+    var spanHtml = '<span style="position:absolute;top:0;left:5px;width:'+img_w+'px;height:'+img_h+'px;" class="imageMask"></span>';
+    $(spanHtml).appendTo(this);
+  });
+  $("#jnBrandList").find(".imageMask").hover(function(){
+    $(this).addClass("imageOver");
+  }, function(){
+    $(this).removeClass("imageOver");
+  });
+});
